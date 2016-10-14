c57c4f7a0518d73cb9d47e8177ec7f2b65bfa6e7
余冲 <yuchong321@126.com>
2016-10-14 10:07:37 +0800

    ylpay

--- src/config/config.php
+++ src/config/config.php
@@ -2,13 +2,13 @@
 return [
 
     //合作身份者id，以2088开头的16位纯数字。
-    'merchant_id'      => '2088421422820144',
+    'merchant_id'      => '898310048164164',
 
     //启动模式，
     'mode'             => 2,
 
     // 安全检验码，以数字和字母组成的32位字符。
-    'key'              => 'bs77r55ip5r8wyt9vposziv64pebmm6z',
+    'key'              => 'c518de1eb7a54b951d0e5b15c8024869',
 
     //私钥
     'private_key'      => 'MIICdQIBADANBgkqhkiG9w0BAQEFAASCAl8wggJbAgEAAoGBALJKJjq9H9jmfia2gEilqPQXZjN+MsivoX9BC2UU80hDhCGN1j8rDysnJlLWMue1HxTJJf8DV2hBfNmqBHqmBMQoEsZ+ITdNsYn/wqLpvPXHFV57/bIIEI2JD72DOx6ZDWgbrp6BXoWVycEvUJ0JJJr+DCCt9LTEyfDXurIUqgZpAgMBAAECgYBkgx8IUGTq8A7AnnS2AAa/DY4Fi6jvsOwYBMB6zRPWcpHEJOVbGVhk2J5nZvCt5lNOcZQlL2oQkZLkV1BNINlgFq7MigGYSQ1V4/YQXjleatR90ABC0QGyfJQaXwmcVAONSkSCaao6uTAex1kwdQP7J+QkK4XDLBCfKBLgVpq1vQJBAOv//5z9UP4c49OD1/m+pnby7L7zQvlfPadIRQXMZnfvmkGlkY/In3IMWcDpUcyvkhE9hDHSOxrEmK9FYXCxfN8CQQDBZiEbs69cU099rihighsYi4la/0JEekljQSCuBIZ3TFfR8aBYwi20akrzUdm4MH5ZWi97j17/CERNVVXla523AkATvpA5JyxehjY9XPt1xpCQxRQviZSh3mj/FNnJeWddQ2uJcHu0JtnIJeZgcGTKlinHTXlA3dDaFXacu1ZCombLAkBrs8WCWNmyr86X7jIdUdlnHOYcYAT2f6d4998MKgb8Tu6lQ0uJwnGThJJC2PVHhvIGLpw80kYT/vWSn5BbWEgXAkA9o7HuOCRtfPz1PW+r32uw+LAc+Kt3cKNSiFgqSlqhUlntOZDHBRxwUkMfRFm5iApFN941uLl+kwvjpFNXX1rT',
